399b5580d03b847fd51cbfcda89043a70ea92a74
tony <t>
2019-12-22 20:36:56 +0800

    laser filter

---
+++ deep_learning/scripts/laser_filter.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+import math
+from math import pi
+
+from sensor_msgs.msg import LaserScan
+
+import time
+import rospkg
+import sys
+rospack = rospkg.RosPack()
+env_path = rospack.get_path('deep_learning')
+
+sys.path.append(env_path+'/env')
+
+
+import rospy
+from std_msgs.msg import String
+
+def filter(output = 360):
+    #data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
+    rospy.init_node('laser_filter', anonymous=True)
+
+    
+    sub = rospy.Subscriber('scan', LaserScan, readLaser, output)
+    rospy.spin()
+
+def readLaser(msg, out):
+    out = 360 / out
+    data = np.array(msg.ranges)
+    new_data = []
+    for i in range(len(data)):
+        if i % out == 0:
+            new_data.append(data[i])
+    msg.ranges = new_data
+    pub.publish(msg)
+    #print (np.array(msg.ranges))
+    #print (len(np.array(msg.ranges)))
+    rospy.loginfo("Laser filted !!")
+
+if __name__ == '__main__':
+    pub = rospy.Publisher('scan_f', LaserScan, queue_size=10)
+    filter(24)
+
+
+
+# class Env():
+#     def __init__(self, action_size):
+#         self.goal_x = 0
+#         self.goal_y = 0
+#         self.heading = 0
+#         self.action_size = action_size
+#         self.initGoal = True
+#         self.get_goalbox = True
+#         self.position = Pose()
+#         self.pub_cmd_vel = rospy.Publisher('/dog/cmd_vel', Twist, queue_size=5)
+#         self.sub_odom = rospy.Subscriber('odom', Odometry, self.getOdometry)
+#         self.reset_proxy = rospy.ServiceProxy('gazebo/reset_world', Empty) # reset_simulation
+#         #self.unpause_proxy = rospy.ServiceProxy('gazebo/unpause_physics', Empty)
+#         #self.pause_proxy = rospy.ServiceProxy('gazebo/pause_physics', Empty)
+#         #self.respawn_goal = Respawn()
+
+#         self.target = MoveTarget()
+#         self.bot = MoveBot()
+
+#         self.ramdom_target = False
+
+#         self.ramdom_bot = False
+#         self.ramdom_bot_rotate = False
+#         self.goalNum = 0
+
+
+#     def getGoalDistace(self):
+#         goal_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y), 2)
+
+#         return goal_distance
+
+#     def getOdometry(self, odom):
+#         self.position = odom.pose.pose.position
+#         orientation = odom.pose.pose.orientation
+#         orientation_list = [orientation.x, orientation.y, orientation.z, orientation.w]
+#         _, _, yaw = euler_from_quaternion(orientation_list)
+
+#         goal_angle = math.atan2(self.goal_y - self.position.y, self.goal_x - self.position.x)
+
+#         heading = goal_angle - yaw
+#         if heading > pi:
+#             heading -= 2 * pi
+
+#         elif heading < -pi:
+#             heading += 2 * pi
+
+#         self.heading = round(heading, 2)
+
+#     def getState(self, scan):
+#         scan_range = []
+#         heading = float(self.heading)
+#         min_range = 0.15
+#         done = False
+
+#         for i in range(len(scan.ranges)):
+#             if scan.ranges[i] == float('Inf'): # pastive infinity       nan > not a number
+#                 scan_range.append(3.5)
+#             elif np.isnan(scan.ranges[i]): #np.isnan(float("nan"))>>True     np.isnan(float("inf"))>>False
+#                 scan_range.append(0)
+#             else:
+#                 scan_range.append(scan.ranges[i])
+
+#         if min_range > min(scan_range) > 0:
+#             done = True
+            
+
+#         current_distance = round(math.hypot(self.goal_x - self.position.x, self.goal_y - self.position.y),2)
+#         current_distance = float(current_distance)
+#         if current_distance < 0.2:
+#             self.get_goalbox = True
+#             done = True
+
+#         return scan_range + [heading, current_distance], done
+
+#     def setReward(self, state, done, action):
+#         yaw_reward = []
+#         current_distance = state[-1]
+#         heading = state[-2]
+	
+
+#         for i in range(7):
+#             angle = -pi / 4 + heading + (pi / 8 * i) + pi / 2
+#             tr = 1 - 4 * math.fabs(0.5 - math.modf(0.25 + 0.5 * angle % (2 * math.pi) / math.pi)[0])
+#             yaw_reward.append(tr)
+
+#         distance_rate = 2 ** (current_distance / self.goal_distance)
+#         reward = ((round(yaw_reward[action] * 5, 2)) * distance_rate)
+#         #print("distance_rate: ",distance_rate)
+#         #print("yaw: ",round(yaw_reward[action] * 5, 2))
+#         Reward = reward
+
+#         if done and not self.get_goalbox:
+#             rospy.loginfo("Collision!!")
+#             Reward = -200
+#             self.pub_cmd_vel.publish(Twist())
+
+#         if self.get_goalbox:
+#             rospy.loginfo("Goal!!")
+#             Reward = 200
+#             self.goalNum = self.goalNum + 1
+#             self.pub_cmd_vel.publish(Twist())
+#             self.goal_x, self.goal_y = self.target.movingAt(self.goalNum,self.ramdom_target)
+#             self.goal_distance = self.getGoalDistace()
+#             #self.get_goalbox = False
+
+#         return Reward
+
+#     def step(self, action):
+#         max_angular_vel = 1.5
+#         ang_vel = ((self.action_size - 1)/2 - action) * max_angular_vel * 0.5
+
+#         vel_cmd = Twist()
+#         vel_cmd.linear.x = 0.3
+#         vel_cmd.angular.z = ang_vel
+#         self.pub_cmd_vel.publish(vel_cmd)
+
+#         data = None
+#         while data is None:
+#             try:
+#                 data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
+#             except:
+#                 pass
+
+#         state, done = self.getState(data)
+#         reward = self.setReward(state, done, action)
+
+#         goal = self.get_goalbox
+#         self.get_goalbox = False
+
+#         return np.asarray(state), reward, done, goal
+
+#     def reset(self):
+#         rospy.wait_for_service('gazebo/reset_world')
+#         try:
+#             self.reset_proxy() #problem
+#         except (rospy.ServiceException) as e:
+#             print("gazebo/reset_simulation service call failed")
+
+#         data = None
+        
+#         while data is None:
+#             try:
+#                 data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
+#             except:
+#                 pass
+
+#         # if self.initGoal:
+#         #     self.goal_x, self.goal_y = self.target.movingAt(self.goalNum)
+#         #     self.initGoal = False
+#         self.goal_x, self.goal_y = self.target.movingAt(self.goalNum, self.ramdom_target)
+
+#         self.goal_distance = self.getGoalDistace()
+#         state, done = self.getState(data)
+
+#         self.bot.movingAt(self.ramdom_bot,self.ramdom_bot_rotate)
+
+#         return np.asarray(state)
+
