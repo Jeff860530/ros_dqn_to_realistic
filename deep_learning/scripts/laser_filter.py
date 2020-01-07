8ee5c5b4f672b7af78d33d8057b62e0e7a8b95a3
jeff0530 <zxc719152@gmail.com>
2020-01-07 14:35:53 +0800

    modify laser filter rotate

--- deep_learning/scripts/laser_filter.py
+++ deep_learning/scripts/laser_filter.py
@@ -18,8 +18,10 @@ sys.path.append(env_path+'/env')
 
 import rospy
 from std_msgs.msg import String
+def rotate(l, n):
+    return l[n:] + l[:n]
 
-def filter(output = 360):
+def filter(output = 360 , real):
     #data = rospy.wait_for_message('/scan', LaserScan, timeout=5)
     rospy.init_node('laser_filter', anonymous=True)
 
@@ -27,19 +29,24 @@ def filter(output = 360):
     sub = rospy.Subscriber('scan', LaserScan, readLaser, output)
     rospy.spin()
 
-def readLaser(msg, out):
+def readLaser(msg, out, real):
     out = 360 / out
     data = np.array(msg.ranges)
     new_data = []
     for i in range(len(data)):
         if i % out == 0:
             new_data.append(data[i])
+    if real:
+        new_data = rotate(new_data,12)
     msg.ranges = new_data
     pub.publish(msg)
+
+    
     #print (np.array(msg.ranges))
     #print (len(np.array(msg.ranges)))
     rospy.loginfo("Laser filted !!")
 
 if __name__ == '__main__':
+    real = rospy.get_param("~real",False)
     pub = rospy.Publisher('scan_f', LaserScan, queue_size=10)
-    filter(24)
+    filter(24,real=real)
